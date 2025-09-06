e860a37fb095e082b93177c6dce9f2bc62670650
RedLeader962 <redleader962@gmail.com>
2025-09-06 13:15:41 -0400

    feat: adjust setup and refine packaging for version management
    - Introduced `setup.py` to dynamically retrieve version from a `version.txt` file.
    - Updated `pyproject.toml` to remove `setuptools-scm` dependency for versioning.
    - Enhanced package discovery and repository structure recommendations, including
      directory exclusions to avoid build issues in mixed Python/ROS2 repositories.
    
    Issue RLRP-83

---
+++ setup.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+from setuptools import setup
+import os
+
+
+def read_version():
+    """ Reads and returns the version information from the semantic-versioning generated file.
+
+    :return: The version information as a string.
+    """
+    version_file = os.path.join(os.path.dirname(__file__), 'version.txt')
+    with open(version_file, 'r') as f:
+        return f.read().strip()
+
+
+setup(
+        version=read_version()
+        )
