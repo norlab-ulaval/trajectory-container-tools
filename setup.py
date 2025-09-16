c43786503288cafb58f0418d8d87ea73cb0319af
RedLeader962 <redleader962@gmail.com>
2025-09-16 11:35:10 -0400

    ci: force build trigger to test CI build config

--- setup.py
+++ setup.py
@@ -5,15 +5,13 @@ import os
 
 
 def read_version():
-    """ Reads and returns the version information from the semantic-versioning generated file.
+    """Reads and returns the version information from the semantic-versioning generated file.
 
     :return: The version information as a string.
     """
-    version_file = os.path.join(os.path.dirname(__file__), 'version.txt')
-    with open(version_file, 'r') as f:
+    version_file = os.path.join(os.path.dirname(__file__), "version.txt")
+    with open(version_file, "r") as f:
         return f.read().strip()
 
 
-setup(
-        version=read_version()
-        )
+setup(version=read_version())
